aa60f17dfe3b12a0ba80773b68f40c3754b55ee6
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-04-05 22:55:43 +0100

    Generate the PNG in grayscale.

--- microdash/core/views.py
+++ microdash/core/views.py
@@ -5,18 +5,16 @@ from django.http import HttpResponse
 
 def get_image():
     size = (600, 800)
-    im = Image.new('RGBA', size, (255, 255, 255, 255))
+    im = Image.new('L', size, 255)
     draw = ImageDraw.Draw(im)
-    color = (0, 0, 0)
     position = (10, 10)
     text = "Hello World!"
-    draw.text(position, text, fill=color)
-    del draw
+    draw.text(position, text, 33)
     return im
 
 
 def station(request, shortcode=None):
     img = get_image()
-    response = HttpResponse(mimetype='image/png')
+    response = HttpResponse(content_type='image/png')
     img.save(response, 'PNG')
     return response
