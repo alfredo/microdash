91063a3550316f931665056e39056fea685ab625
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-04-05 20:59:54 +0100

    Return a PNG image.

--- microdash/core/views.py
+++ microdash/core/views.py
@@ -1,6 +1,22 @@
-from django.template.response import TemplateResponse
+from PIL import Image, ImageDraw
+
+from django.http import HttpResponse
+
+
+def get_image():
+    size = (600, 800)
+    im = Image.new('RGBA', size, (255, 255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    color = (0, 0, 0)
+    position = (10, 10)
+    text = "Hello World!"
+    draw.text(position, text, fill=color)
+    del draw
+    return im
 
 
 def home(request):
-    context = {}
-    return TemplateResponse(request, 'home.html', context)
+    img = get_image()
+    response = HttpResponse(mimetype='image/png')
+    img.save(response, 'PNG')
+    return response
