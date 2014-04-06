119d35c154fb7da1485fdcd5ce35fc49a9de821e
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-04-06 12:30:43 +0100

    Removed unused bash script.

--- weather/weather-display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-rm weather-script-output.png
-wget -O "/mnt/us/weather/weather-script-output.png" "http://microdash.herokuapp.com/FOG/"
-eips -c
-eips -c
-eips -g weather-script-output.png
