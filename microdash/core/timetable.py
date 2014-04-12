736b92ee87fef7cf11c0d741aed7a15ec3e0c37e
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-04-12 13:23:34 +0100

    Update timetable generator.

--- microdash/core/timetable.py
+++ microdash/core/timetable.py
@@ -15,13 +15,16 @@ URL_PREFIX = ('http://www.abelliogreateranglia.co.uk/travel-information'
 STATION_COMMUTE = 60 * 8
 
 
-def get_datetime(now, time_str):
+def parse_time(time_str):
+    """Generates today datetime from a HH:MM formatted time."""
+    now = datetime.now()
     hour_str, minutes_str = time_str.split(':')
     return datetime(
         now.year, now.month, now.day, int(hour_str), int(minutes_str))
 
 
 def parse_response(content, valid_stations):
+    """Parse the content response."""
     columns = ['destination', 'time', 'status', 'origin', 'operator']
     soup = BeautifulSoup(content)
     timetable = []
@@ -36,29 +39,32 @@ def parse_response(content, valid_stations):
     return timetable
 
 
-def get_schedule(raw_timetable):
-    now = datetime.now()
-    for item in raw_timetable:
-        item['destination'] = item['destination'].upper()
-        train_datetime = get_datetime(now, item['time'])
-        diff = train_datetime - now
-        item['datetime'] = train_datetime
-        home_departure = diff.total_seconds() - STATION_COMMUTE
-        item['home_departure'] = home_departure
-        if home_departure > 0:
-            depart_in = 'Depart in %s mins.' % int((home_departure / 60))
-        else:
-            depart_in = ('Unlikely. Train in %s mins.'
-                         % int(diff.total_seconds() / 60))
-        item['depart_in'] = depart_in
-    return raw_timetable
+def get_train_datetime(item):
+    """Parse the train time of arrival."""
+    time_str = item['status'] if ':' in item['status'] else item['time']
+    return parse_time(time_str)
+
+
+def update_schedule(item):
+    """Add more detail to the schedule.."""
+    item['destination'] = item['destination'].upper()
+    train_datetime = get_train_datetime(item)
+    time_diff = train_datetime - datetime.now()
+    home_departure = time_diff.total_seconds() - STATION_COMMUTE
+    if home_departure > 0:
+        depart_in = 'Depart in %s mins.' % int((home_departure / 60))
+    else:
+        minutes = int(time_diff.total_seconds() / 60)
+        depart_in = ('Unlikely. Train in %s mins.' % minutes)
+    item['depart_in'] = depart_in
+    return item
 
 
 def get_timetable(shortcode, valid_stations):
-    """Prepares the timetable."""
+    """Prepares the timetable to be rendered."""
     url = '%s%s' % (URL_PREFIX, shortcode)
     response = requests.get(url, headers=HEADERS)
     if not response.status_code == 200:
         response = requests.get(url, headers=HEADERS)
     raw_timetable = parse_response(response.content, valid_stations)
-    return get_schedule(raw_timetable)
+    return map(update_schedule, raw_timetable)
